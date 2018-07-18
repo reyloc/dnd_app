5f2681b118cfee2fa348422a590bb5fc86173180
Jason Colyer <jcolyer2007@gmail.com>
2018-07-18 13:29:34 -0500

    Added seeds for realms

---
+++ db/seeds/006_realms.rb
@@ -0,0 +1,38 @@
+Realm.create([
+  {:name => "The Forgotten Realms",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Greyhawk",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Dragonlance",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Eberron",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Nonhuman",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Celtic Pantheon",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Greek Pantheon",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Egyptian Pantheon",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Norse Pantheon",
+   :is_public => true,
+   :user_id => 1
+  }
+])
