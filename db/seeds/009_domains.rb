370d0739603974bdc9ad4a0f5ce3ac68307aad2e
Jason Colyer <jcolyer2007@gmail.com>
2018-07-18 14:16:53 -0500

    Seeded domains

---
+++ db/seeds/009_domains.rb
@@ -0,0 +1,47 @@
+Domain.create([
+  {:name => "Death",
+   :description => "Concerned with the forces that cause death, as well as the negative energy that gives rise to undead creatures",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Knowledge",
+   :description => "Values learning and understanding above all",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Life",
+   :description => "Focuses on the vibrant positive energy that sustains all life.",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Light",
+   :description => "Promote the ideals of rebirth and renewal, truth, vigilance, and beauty",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Nature",
+   :description => "Reveres nature as a whole",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "No Clerics",
+   :description => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Tempest",
+   :description => "Govern storms, sea, and sky",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Trickery",
+   :description => "Mischief-makers and instigators who stand as a constant challenge to the accepted order",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "War",
+   :description => "Watches over warriors and reward them for their great deeds",
+   :is_public => true,
+   :user_id => 1
+  }
+])
