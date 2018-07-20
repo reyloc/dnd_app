0ae9be453345f78324a108656f06d7a07f505fba
Jason Colyer <jcolyer2007@gmail.com>
2018-07-20 06:18:24 -0500

    Modified tool structure, seeded tool

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -3,7 +3,7 @@
 # tool model
 class Tool < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  belongs_to :image
+  belongs_to :image, optional: true
   belongs_to :user
 
   def public?
--- db/migrate/20180717224447_create_tools.rb
+++ db/migrate/20180717224447_create_tools.rb
@@ -3,15 +3,16 @@ class CreateTools < ActiveRecord::Migration[5.2]
     create_table :tools do |t|
       t.string :name, null: false
       t.string :description
-      t.string :type, null: false, default: ''
-      t.integer :weight
+      t.string :subtype, null: false, default: ''
+      t.float :weight
       t.integer :cost
+      t.string :currency
       t.references :image
       t.boolean :is_public, default: false
       t.references :user
       t.timestamps
     end
     add_index :tools, :name, unique: true
-    add_index :tools, :type
+    add_index :tools, :subtype
   end
 end
