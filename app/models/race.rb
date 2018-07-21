d7dc378cf2985073e9b0c9372e1520cbec86c1bc
Jason Colyer <jcolyer2007@gmail.com>
2018-07-21 17:06:01 -0500

    Worked on race controller

--- app/models/race.rb
+++ app/models/race.rb
@@ -11,6 +11,7 @@ class Race < ApplicationRecord
   has_many :race_languages
   has_many :traits
   has_many :racial_traits
+  has_many :languages, through: :race_languages
 
   def public?
     is_public
