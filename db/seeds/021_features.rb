a66beb895028cb7e1cbaba5d7545cec4d223edc6
Jason Colyer <jcolyer2007@gmail.com>
2018-07-21 19:19:51 -0500

    Seeded features

---
+++ db/seeds/021_features.rb
@@ -0,0 +1,77 @@
+Feature.create([
+  {:name => "Shelter of the Faithful",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "False Identity",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Criminal Contact",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "By Popular Demand",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Rustic Hospitality",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Guild Membership",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Discovery",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Position of Privilege",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Wanderer",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Researcher",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Ship's Passage",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Military Rank",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "City Secrets",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Retainers",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  },
+  {:name => "Bad Reputation",
+   :table_id => "",
+   :is_public => true,
+   :user_id => 1
+  }
+])
