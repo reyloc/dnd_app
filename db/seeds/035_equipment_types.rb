926066556472b45ece14f1c837975a55cf936788
Jason Colyer <jcolyer2007@gmail.com>
2018-07-23 12:52:13 -0500

    Added tool description to seeds

--- db/seeds/035_equipment_types.rb
+++ db/seeds/035_equipment_types.rb
@@ -18,7 +18,7 @@ EquipmentType.create([
    :description => "A shield is made from wood or metal and is carried in one hand. Wielding a shield increases your Armor Class by 2. You can benefit from only one shield at a time."
   },
   {:name => "Tool",
-   :description => "",
+   :description => "A tool helps you to do something you couldn't otherwise do, such as craft or repair an item, forge a document, or pick a lock. Your race, class, background, or feats give you proficiency with certain tools. Proficiency with a tool allows you to add your proficiency bonus to any ability check you make using that tool. Tool use is not tied to a single ability, since proficiency with a tool represents broader knowledge of its use. For example, the DM might ask you to make a Dexterity check to carve a fine detail with your woodcarver's tools, or a Strength check to make something out of particularly hard wood.",
   },
   {:name => "Trinket",
    :description => "A simple item lightly touched by mystery."
