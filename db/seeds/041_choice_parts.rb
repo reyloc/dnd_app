376acafccfcc56b3ff3ab4a3ba5a48632419c7b2
Jason Colyer <jcolyer2007@gmail.com>
2018-07-31 16:18:57 -0500

    Cleaned up background equipment view, seeded more choice_parts

--- db/seeds/041_choice_parts.rb
+++ db/seeds/041_choice_parts.rb
@@ -38,5 +38,101 @@ ChoicePart.create([
   {:equipment_choice_id => 7,
    :equipment_id => 45,
    :quantity => 15
+  },
+  {:equipment_choice_id => 8,
+   :equipment_id => 135,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 9,
+   :equipment_id => 19,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 10,
+   :equipment_id => 218,
+   :quantity => 10
+  },
+  {:equipment_choice_id => 10,
+   :equipment_id => 219,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 10,
+   :equipment_id => 220,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 10,
+   :equipment_id => 221,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 11,
+   :equipment_id => 217,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 12,
+   :equipment_id => 45,
+   :quantity => 15
+  },
+  {:equipment_choice_id => 13,
+   :equipment_id => 138,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 14,
+   :equipment_id => 222,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 15,
+   :equipment_id => 217,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 16,
+   :equipment_id => 45,
+   :quantity => 15
+  },
+  {:equipment_choice_id => 17,
+   :equipment_id => 27,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 18,
+   :equipment_id => 223,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 18,
+   :equipment_id => 224,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 19,
+   :equipment_id => 132,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 20,
+   :equipment_id => 217,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 21,
+   :equipment_id => 45,
+   :quantity => 15
+  },
+  {:equipment_choice_id => 22,
+   :equipment_id => 1,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 23,
+   :equipment_id => 187,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 24,
+   :equipment_id => 175,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 25,
+   :equipment_id => 133,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 26,
+   :equipment_id => 217,
+   :quantity => 1
+  },
+  {:equipment_choice_id => 27,
+   :equipment_id => 45,
+   :quantity => 10
   }
 ])
